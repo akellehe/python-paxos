caf52b4d3e846525aee71940e794418eab4c1d68
Andrew Kelleher <andrew.kelleher@venmo.com>
2018-10-16 23:21:11 -0400

    Cleanup

--- state-machine/client.py
+++ state-machine/client.py
@@ -10,7 +10,6 @@ from settings import AGENT_URL, AGENT_PORTS
 failures = collections.defaultdict(int)
 
 # Distinguished proposer/learner
-port = random.choice(AGENT_PORTS)
 url = AGENT_URL + ':8888/write'
 
 def sync():
@@ -27,10 +26,8 @@ def sync():
     if 200 <= response.status_code < 300:
         sys.stdout.write('.')
     else:
-        failures[url] += 1
         sys.stdout.write('x')
     sys.stdout.flush()
 
 sync()
-print("Failures", failures)
 
